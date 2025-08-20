5e12fa48593c4e2d67b90e8025e1f612ce82553f
bernardomoraisapp <thevolumelive@gmail.com>
2025-08-20 00:23:46 -0300

    Implantação do Projection

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -2,9 +2,12 @@ package com.devsuperior.dslist.controllers;
 
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,17 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDto> findByList(@PathVariable Long listId) {
+        List<GameMinDto> result = gameService.findByList(listId);
         return result;
     }
 
